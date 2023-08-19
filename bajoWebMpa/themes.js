d14a0ab6b9afab02d6b049ef8ebd2c0d116ed293
Ardhi Lukianto <ardhi@lukianto.com>
2023-08-19 10:06:06 +0700

    Helper renamed

--- bajoWebMpa/themes.js
+++ bajoWebMpa/themes.js
@@ -5,15 +5,15 @@ const items = ['cerulean', 'cosmo', 'cyborg', 'darkly', 'flatly', 'journal',
 
 async function themes () {
   const { importPkg } = this.bajo.helper
-  const { virtualsDir } = this.bajoWebStatic.helper
+  const { virtualDir } = this.bajoWebStatic.helper
   const { map, upperFirst } = await importPkg('lodash-es')
   return map(items, name => {
     return {
       name,
       framework: 'bootstrap3',
       description: `Bootswatch 5.3 - ${upperFirst(name)}`,
-      script: `${virtualsDir('bajoWebMpaBootstrap')}/bs5/js/bootstrap.bundle.min.js`,
-      css: `${virtualsDir('bajoWebMpaBootswatch')}/dist/${name}/bootstrap.min.css`
+      script: `${virtualDir('bajoWebMpaBootstrap')}/bs5/js/bootstrap.bundle.min.js`,
+      css: `${virtualDir('bajoWebMpaBootswatch')}/dist/${name}/bootstrap.min.css`
     }
   })
 }
